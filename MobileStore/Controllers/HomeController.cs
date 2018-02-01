703ae07b09410c5e3ed16c88b7535e2d874d6051
Alex Zakharov <newleha1@gmail.com>
2018-02-01 11:54:02 +0700

    Finish MobileStore

---
+++ MobileStore/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MobileStore.Models;
+
+namespace MobileStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        MobileContext db;
+        public HomeController (MobileContext context)
+        {
+            db = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(db.Phones.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Buy (int id)
+        {
+            ViewBag.PhoneId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy (Order order)
+        {
+            db.Orders.Add(order);
+            db.SaveChanges();
+            return "Спасибо," + order.User + ", за покупку!";
+        }
+   
+    }
+}
