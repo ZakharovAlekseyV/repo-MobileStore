73da173af56cfb85f63ae1a2c521e287bb6b141d
Alex Zakharov <newleha1@gmail.com>
2018-02-02 10:29:18 +0700

    add gitignore

---
+++ MobileStore/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MobileStore.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MobileStore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<MobileContext>();
+                    SampleData.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seding the database.");
+                }
+            }
+            host.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
